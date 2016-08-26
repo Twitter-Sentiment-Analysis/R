24becea9c06f6a42c86d7298c812342741032723
Asmita Poddar <asmita_poddar@yahoo.co.in>
2016-08-26 23:15:06 +0530

    Create Word database.R

---
+++ Word database.R
@@ -0,0 +1,6 @@
+#Adding words to positive and negative databases
+
+
+pos.words=c(pos.words, 'Congrats', 'prizes', 'prize', 'thanks', 'thnx', 'Grt', 'gr8', 'plz', 'trending', 'recovering', 'brainstorm', 'leader')
+neg.words = c(neg.words, 'Fight', 'fighting', 'wtf', 'arrest', 'no', 'not')
+
