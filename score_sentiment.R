3d855e7fe6c953e13e3768dd2f4f98ea4c3601b9
Asmita Poddar <asmita_poddar@yahoo.co.in>
2016-08-26 23:37:22 +0530

    Add files via upload

--- score_sentiment.R
+++ score_sentiment.R
@@ -1,34 +1,32 @@
-score.sentiment = function(sentences, pos.words, neg.words)
+score.sentiment = function(sentences, pos.words, neg.words, .progress='none')
 {
-	 require(plyr)
-	 require(stringr)
-	
-	 # we got a vector of sentences. plyr will handle a list or a vector as an "l" for us
-	 # we want a simple array of scores back, so we use "l" + "a" + "ply" = laply:
-	 scores = lapply(sentences, function(sentence, pos.words, neg.words) {
-	 	
-	 	 # clean up sentences with R's regex-driven global substitute, gsub():
-	 	 sentence = gsub('[[:punct:]]', ' ', sentence)
-	 	 sentence = gsub('[[:cntrl:]]', '', sentence)
-	 	 sentence = gsub('\\d+', '', sentence)
-	 	 # and convert to lower case:
-	 	 sentence = tolower(sentence)
-	 	 # split into words. str_split is in the stringr package
-	 	 word.list = strsplit(sentence, '\\s+')
-	 	 # sometimes a list() is one level of hierarchy too much
-	 	 words = unlist(word.list)
-	 	 # compare our words to the dictionaries of positive & negative terms
-	 	 pos.matches = match(words, pos.words)
-	 	 neg.matches = match(words, neg.words)
-	
-	 	 # match() returns the position of the matched term or NA
-	 	 # we just want a TRUE/FALSE:
-	 	 pos.matches = !is.na(pos.matches)
-	 	 neg.matches = !is.na(neg.matches)
-	 	 # and conveniently enough, TRUE/FALSE will be treated as 1/0 by sum():
-	 	 score = sum(pos.matches) - sum(neg.matches)
-	 	 return(score)
-	 }, pos.words, neg.words )
-	 scores.df = data.frame(score=scores, text=sentences)
-	 return(scores.df)
+	require(plyr)
+	require(stringr)
+	list=lapply(sentences, function(sentence, pos.words, neg.words)
+	{
+		sentence = gsub('[[:punct:]]',' ',sentence)
+		sentence = gsub('[[:cntrl:]]','',sentence)
+		sentence = gsub('\\d+','',sentence)
+		sentence = gsub('\n','',sentence)
+
+		sentence = tolower(sentence)
+		word.list = str_split(sentence, '\\s+')
+		words = unlist(word.list)
+		pos.matches = match(words, pos.words)
+		neg.matches = match(words, neg.words)
+		pos.matches = !is.na(pos.matches)
+		neg.matches = !is.na(neg.matches)
+		pp=sum(pos.matches)
+		nn = sum(neg.matches)
+		score = sum(pos.matches) - sum(neg.matches)
+		list1=c(score, pp, nn)
+		return (list1)
+	}, pos.words, neg.words)
+	score_new=lapply(list, `[[`, 1)
+	pp1=score=lapply(list, `[[`, 2)
+	nn1=score=lapply(list, `[[`, 3)
+
+	scores.df = data.frame(score=score_new, text=sentences, Positive=pp1, Negative=nn1)
+	return(scores.df)
 }
+
