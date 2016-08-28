32f3f0bcaa838705962c7818911260e7aa89a1b4
Asmita Poddar <asmita_poddar@yahoo.co.in>
2016-08-29 00:14:54 +0530

    Update func_on_tweet.R

--- func_on_tweet.R
+++ func_on_tweet.R
@@ -5,13 +5,28 @@ result = score.sentiment(df$text, pos.words, neg.words)
 
  library(reshape)
 #Creating a copy of result data frame
-test=result
+test1=result[[1]]
+test2=result[[2]]
+test3=result[[3]]
+
+#Creating three different data frames for Score, Positive and Negative
 #Removing text column from data frame
- result$text=NULL
+test1$text=NULL
+test2$text=NULL
+test3$text=NULL
 #Storing the first row(Containing the sentiment scores) in variable q
-q=result[1,]
-qq=melt(q, ,var='Score') 
-qq['Score'] = NULL
+q1=test1[1,]
+q2=test2[1,]
+q3=test3[1,]
+qq1=melt(q1, ,var='Score')
+qq2=melt(q2, ,var='Positive')
+qq3=melt(q3, ,var='Negative') 
+qq1['Score'] = NULL
+qq2['Positive'] = NULL
+qq3['Negative'] = NULL
 #Creating data frame
-table = data.frame(Text=test$text, Score=qq)
+table1 = data.frame(Text=result[[1]]$text, Score=qq1)
+table2 = data.frame(Text=result[[2]]$text, Score=qq2)
+table3 = data.frame(Text=result[[3]]$text, Score=qq3)
+
 
