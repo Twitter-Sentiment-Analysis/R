436e88026a7e4b7751bca45c83ed564dfcd9dbb1
Sonali Agrawal <sonali9696@gmail.com>
2016-09-13 00:14:10 +0530

    Update emoticon.R

--- emoticon.R
+++ emoticon.R
@@ -1,5 +1,7 @@
 #s <- searchTwitter('#emoticons', cainfo="cacert.pem")
+#converts to data frame
 df <- do.call("rbind", lapply(rahul.tweets, as.data.frame))
+#remove odd characters
 df$text <- sapply(df$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
-sample<-df$text
-
+df$text = gsub("(f|ht)tp(s?)://(.*)[.][a-z]+", "", df$text)
+sample <- df$text
