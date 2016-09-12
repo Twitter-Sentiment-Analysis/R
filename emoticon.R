2de3e283ea93b86d50f93477b329bf4eed0ad63a
Sonali Agrawal <sonali9696@gmail.com>
2016-09-12 23:36:12 +0530

    sample

--- emoticon.R
+++ emoticon.R
@@ -1,4 +1,5 @@
 #s <- searchTwitter('#emoticons', cainfo="cacert.pem")
 df <- do.call("rbind", lapply(rahul.tweets, as.data.frame))
 df$text <- sapply(df$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
+sample<-df$text
 
