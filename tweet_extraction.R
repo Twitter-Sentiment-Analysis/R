72c8c84ec1887735ad8eae2a2f286fcb574d672d
Sonali Agrawal <sonali9696@gmail.com>
2016-09-12 23:14:59 +0530

    Multiple words

--- tweet_extraction.R
+++ tweet_extraction.R
@@ -1,2 +1,5 @@
 #Extracting tweets
-rahul.tweets = searchTwitter("@rahul", n=1500)
+rahul.tweets = searchTwitter("@rahul", n=1500)  
+
+#For searching multiple words together
+searchTwitter('Donald+Trump',lang="en",n=N)
