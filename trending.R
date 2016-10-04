3774f90ea8d0db3a4508956b1eb0bda5a638c990
Sonali Agrawal <sonali9696@gmail.com>
2016-10-04 17:11:06 +0530

    Update trending.R

--- trending.R
+++ trending.R
@@ -2,16 +2,22 @@
 
 library(TwitteR)
 
-#Get woeid of all locations on Earth
+#take input of place
+#get woeid
+#print table of hashtag, url
+
+#Let input Ottawa
 a_trends = availableTrendLocations()
-#Get woeid from input location
-woeid=a_trends[3,which(a_trends$name == 'Ottawa')]
+woeid = a_trends[3,which(a_trends$name=="Ottawa")]
+canada_trends = getTrends(woeid)
+trends = canada_trends[1:2]
+
 
 
 #Get trending tweets worldwide
 today_trends = getTrends(1);
-#head(today_trends);
 
+#To clean data and remove Non English words: (not required)
 dat <- cbind(getTrends(1)$name)
 dat2 <- unlist(strsplit(dat, split=", "))
 dat3 <- grep("dat2", iconv(dat2, "latin1", "ASCII", sub="dat2"))
