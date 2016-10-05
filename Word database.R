5965342800a250927605c37be6b7f2c4077533bd
Sonali Agrawal <sonali9696@gmail.com>
2016-10-05 11:23:44 +0530

    Update Word database.R

--- Word database.R
+++ Word database.R
@@ -1,6 +1,9 @@
+
+#After downloading the hui.lui.pos and hui.lui.neg, mention below the location of the file
+pos.words = scan('C:/Users/hp/Documents/positive-words.txt', what='character', comment.char=';')
+neg.words = scan('C:/Users/hp/Documents/negative-words.txt', what='character', comment.char=';')
+
 #Adding words to positive and negative databases
-
-
 pos.words=c(pos.words, 'Congrats', 'prizes', 'prize', 'thanks', 'thnx', 'Grt', 'gr8', 'plz', 'trending', 'recovering', 'brainstorm', 'leader')
 neg.words = c(neg.words, 'Fight', 'fighting', 'wtf', 'arrest', 'no', 'not')
 
